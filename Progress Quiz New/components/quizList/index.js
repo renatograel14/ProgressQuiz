fe98adc9ef13f9940c9a82c94b18b964aa814cd7
Renato Teixeira <rteixeir@progress.com>
2015-09-18 16:46:32 +0000

    Changes from Screen Builder

---
+++ Progress Quiz New/components/quizList/index.js
@@ -0,0 +1,66 @@
+'use strict';
+
+app.quizList = kendo.observable({
+    onShow: function() {},
+    afterShow: function() {}
+});
+
+// START_CUSTOM_CODE_quizList
+// END_CUSTOM_CODE_quizList
+(function(parent) {
+    var dataProvider = app.data.defaultProvider,
+        flattenLocationProperties = function(dataItem) {
+            var propName, propValue,
+                isLocation = function(value) {
+                    return propValue && typeof propValue === 'object' &&
+                        propValue.longitude && propValue.latitude;
+                };
+
+            for (propName in dataItem) {
+                if (dataItem.hasOwnProperty(propName)) {
+                    propValue = dataItem[propName];
+                    if (isLocation(propValue)) {
+                        // Location type property
+                        dataItem[propName] =
+                            kendo.format('Latitude: {0}, Longitude: {1}',
+                                propValue.latitude, propValue.longitude);
+                    }
+                }
+            }
+        },
+        dataSourceOptions = {
+            type: 'everlive',
+            transport: {
+                typeName: 'Activities',
+                dataProvider: dataProvider
+            },
+
+            change: function(e) {
+                var data = this.data();
+                for (var i = 0; i < data.length; i++) {
+                    var dataItem = data[i];
+
+                    flattenLocationProperties(dataItem);
+                }
+            },
+            schema: {
+                model: {
+                    fields: {
+                        'Text': {
+                            field: 'Text',
+                            defaultValue: ''
+                        },
+                    }
+                }
+            },
+        },
+        dataSource = new kendo.data.DataSource(dataSourceOptions),
+        quizListModel = kendo.observable({
+            dataSource: dataSource
+        });
+
+    parent.set('quizListModel', quizListModel);
+})(app.quizList);
+
+// START_CUSTOM_CODE_quizListModel
+// END_CUSTOM_CODE_quizListModel
